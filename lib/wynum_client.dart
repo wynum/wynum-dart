4653a7d25c59bb6f9c0182f2dd73df7dd60fb01c
Suraj Patil <surajp815@gmail.com>
2019-05-25 17:47:26 +0530

    first commit  🔥

---
+++ lib/wynum_client.dart
@@ -0,0 +1,96 @@
+library wynum_client;
+
+import 'dart:io';
+
+import 'package:meta/meta.dart';
+import 'package:dio/dio.dart';
+import 'package:path/path.dart';
+
+/// Wynum Client.
+class Client {
+  final String _secret;
+  final String _token;
+
+  final Dio _dio = Dio();
+
+  final String _baseSchmaUrl = "https://api.wynum.com/component";
+  final String _baseDataUrl = "https://api.wynum.com/data";
+
+  String _schmaUrl, _dataUrl;
+
+  Client._(this._secret, this._token) {
+    this._schmaUrl = "$_baseSchmaUrl/$_token";
+    this._dataUrl = "$_baseDataUrl/$_token";
+  }
+
+  factory Client.create({@required secret, @required token}) =>
+      Client._(secret, token);
+
+  Future<Map<String, dynamic>> getSchema() async {
+    final response = await _dio.get(_schmaUrl);
+    final data = response.data;
+    return data;
+  }
+
+  Future<Map<String, dynamic>> get() async {
+    final response = await _dio.get(_dataUrl);
+    final data = response.data;
+    return data;
+  }
+
+  Future<Map<String, dynamic>> post(Map<String, dynamic> data) async {
+    assert(data != null);
+
+    // check for file
+    final hasFile = data.keys.any((key) => data[key] is File);
+
+    Response response;
+    if (hasFile) {
+      final formData = _prepareFormDataForFile(data);
+      response = await _dio.post(_dataUrl, data: formData);
+    } else {
+      response = await _dio.post(_dataUrl, data: data);
+    }
+
+    return response.data;
+  }
+
+  Future<void> update(Map<String, dynamic> data) async {
+    assert(data != null);
+    final response = await _dio.put(_dataUrl, data: data);
+    return response.data;
+  }
+
+  FormData _prepareFormDataForFile(Map<String, dynamic> data) {
+    final files = [];
+
+    for (var key in data.keys) {
+      if (data[key] is File) {
+        File file = data[key];
+        var fileName = basename(file.path);
+        data[key] = fileName;
+        files.add(UploadFileInfo(file, fileName));
+      }
+    }
+
+    var transformedData = {};
+    for (var key in data.keys) {
+      var val = data[key];
+      switch (val.runtimeType) {
+        case String:
+          transformedData['"$key"'] = '"$val"';
+          break;
+        case List:
+          val = val.map((tmp) => tmp is String ? '"$tmp"' : tmp).toList();
+          transformedData['"$key"'] = val;
+          break;
+        default:
+          transformedData['"$key"'] = val;
+      }
+    }
+
+    final formData =
+        FormData.from({'inputdata': transformedData, 'files': files});
+    return formData;
+  }
+}
---
+++ test/wynum_client_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:wynum_client/wynum_client.dart';
+
+void main() {
+  test('adds one to input values', () {
+    // final calculator = Calculator();
+    // expect(calculator.addOne(2), 3);
+    // expect(calculator.addOne(-7), -6);
+    // expect(calculator.addOne(0), 1);
+    // expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+    // final client = Client.create(secret: "", token: '8979947');
+    // expectAsync0(() => client.getSchema());
+  });
+}
